710eda7ec938f637e6c06a62cd5f548c32123256
Piotr <boquete37@gmail.com>
2018-01-23 12:33:43 +0100

    Move views to api_views

--- smoger/api_urls.py
+++ smoger/api_urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from smoger import views
+from smoger import api_views
 from django.conf.urls import include
 
 
 urlpatterns = [
-    path('sensors/', views.SensorList.as_view()),
-    path('sensors/<int:pk>/', views.SensorDetail.as_view()),
-    path('sensors/<int:sensor_id>/data/', views.SensorDataList.as_view()),
-    path('sensors/<int:sensor_id>/data/<int:pk>/', views.SensorDataDetail.as_view()),
+    path('sensors/', api_views.SensorList.as_view()),
+    path('sensors/<int:pk>/', api_views.SensorDetail.as_view()),
+    path('sensors/<int:sensor_id>/data/', api_views.SensorDataList.as_view()),
+    path('sensors/<int:sensor_id>/data/<int:pk>/', api_views.SensorDataDetail.as_view()),
     path('auth/', include('rest_framework.urls')),
 ]
--- smoger/views.py
+++ smoger/views.py
@@ -7,28 +7,3 @@ from rest_framework import permissions
 
 from smoger.models import Sensor, SensorData
 from smoger.serializers import SensorSerializer, SensorDataSerializer, SensorDetailSerializer
-
-
-class SensorList(generics.ListAPIView):
-    queryset = Sensor.objects.all()
-    serializer_class = SensorSerializer
-
-
-class SensorDetail(generics.RetrieveAPIView):
-    queryset = Sensor.objects.all()
-    serializer_class = SensorDetailSerializer
-
-
-class SensorDataList(generics.ListCreateAPIView):
-    serializer_class = SensorDataSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def get_queryset(self):
-        return SensorData.objects.filter(sensor_id=self.kwargs['sensor_id'])
-
-
-class SensorDataDetail(generics.RetrieveAPIView):
-    serializer_class = SensorDataSerializer
-
-    def get_queryset(self):
-        return SensorData.objects.filter(sensor_id=self.kwargs['sensor_id'])
